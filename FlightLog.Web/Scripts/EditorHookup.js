801ab2b6ab8fbce506e57adf4347151bcb1819b7
Jan Hebnes <jan.hebnes@gmail.com>
2011-11-03 02:09:43 +0100

    Dato format skal over til yyyy-mm-dd så validering fungerer på prod.

--- FlightLog.Web/Scripts/EditorHookup.js
+++ FlightLog.Web/Scripts/EditorHookup.js
@@ -5,15 +5,17 @@ $(document).ready(function () {
 	function getDateYymmdd(value) {
 		if (value == null)
 			return null;
-		return $.datepicker.parseDate("dd-mm-yy", value);
+		return $.datepicker.parseDate("yy-mm-dd", value);
 	}
 	$('.date').each(function () {
 		var minDate = getDateYymmdd($(this).data("val-rangedate-min"));
 		var maxDate = getDateYymmdd($(this).data("val-rangedate-max"));
 		$(this).datepicker({
-			dateFormat: "dd-mm-yy",  // hard-coding dk date format, but could embed this as an attribute server-side (based on the current culture)
-			minDate: minDate,
-			maxDate: maxDate
+			dateFormat: "yy-mm-dd",  // hard-coding dk date format, but could embed this as an attribute server-side (based on the current culture)
+			firstDay: 1,
+            minDate: minDate,
+			maxDate: maxDate,
+			dayNamesMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø']
 		});
 	});
 });
