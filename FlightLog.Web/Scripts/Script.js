f9626a7087b9fe4933b7805f9e504c30f17fe9b0
Jan Hebnes <jan.hebnes@gmail.com>
2012-04-12 00:08:23 +0200

    Edit link that toggles the Comment form

--- FlightLog.Web/Scripts/Script.js
+++ FlightLog.Web/Scripts/Script.js
@@ -3,4 +3,19 @@
         parent.history.back();
         return false;
     });
+
+    $('a.editComment').click(function (e) {
+        e.preventDefault();
+
+        var $this = $(this).closest("td");
+        console.log($this);
+
+        //$this.children("span:first").toggle();
+        $this.children("span:last").toggle();
+
+        return false;
+    });
+
+
 });
+
