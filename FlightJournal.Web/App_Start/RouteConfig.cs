053b09735a1e7ff56df53adaea99777467a7e456
Jan Hebnes <jan.hebnes@gmail.com>
2014-06-26 00:11:18 +0200

    Default route set to report index (Dagsrapporter)

--- FlightJournal.Web/App_Start/RouteConfig.cs
+++ FlightJournal.Web/App_Start/RouteConfig.cs
@@ -81,7 +81,7 @@ namespace FlightJournal.Web{
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Report", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
