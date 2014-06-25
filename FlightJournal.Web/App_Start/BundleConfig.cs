bba39e7877f5376a88fed62176ba7fb68402924d
Jan Hebnes <jan.hebnes@gmail.com>
2014-06-26 01:27:35 +0200

    Added Tablesorter Js from NuGet
    But removed native file paths and style.css added to site.css in min
    version

--- FlightJournal.Web/App_Start/BundleConfig.cs
+++ FlightJournal.Web/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace FlightJournal.Web
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery.tablesorter.min.js"));    // NuGet installed
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
