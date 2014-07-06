644aa841b63a9ccf5ea2366201b69650c5389413
Jan Hebnes <jan.hebnes@gmail.com>
2014-07-07 01:34:24 +0200

    Layout for the fields and visualizing chart and table have been worked on

---
+++ FlightJournal.Web/Scripts/jquery.editorhookup.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="jquery-1.4.4.js" />
+/// <reference path="jquery-ui.js" />
+
+$(document).ready(function () {
+	function getDateYymmdd(value) {
+		if (value == null)
+			return null;
+		return $.datepicker.parseDate("yy-mm-dd", value);
+	}
+	$('.dateISO').each(function () {
+		var minDate = getDateYymmdd($(this).data("val-rangedate-min"));
+		var maxDate = getDateYymmdd($(this).data("val-rangedate-max"));
+		$(this).datepicker({
+			dateFormat: "yy-mm-dd",  // hard-coding dk date format, but could embed this as an attribute server-side (based on the current culture)
+			firstDay: 1,
+            minDate: minDate,
+			maxDate: maxDate,
+			dayNamesMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø']
+		});
+	});
+});
