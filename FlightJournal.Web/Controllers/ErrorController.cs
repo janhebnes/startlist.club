6c49bfdeaa4b7c43ba8c3915fbe88dcf02d61205
Jan Hebnes <jan.hebnes@gmail.com>
2014-07-24 14:31:46 +0200

    Trials for a 500 Error reporting page but problems with Server.GetLastError

--- FlightJournal.Web/Controllers/ErrorController.cs
+++ FlightJournal.Web/Controllers/ErrorController.cs
@@ -14,10 +14,30 @@ namespace FlightJournal.Web.Controllers
             return View();
         }
 
-        public ActionResult RuntimeError()
-        {
-            Response.StatusCode = 500;
-            return View();
-        }
+    // The server.GetLastError is currently not preserved... so the 500 handling is currently disabled
+    ////    public ActionResult Runtime()
+    ////    {
+    ////        Response.StatusCode = 500;
+    ////        return View();
+    ////    }
+
+    ////    public ActionResult TestRuntimeError()
+    ////    {
+    ////        throw new Exception("Testing Runtime Error - the server.GetLastError is currently not preserved... so the 500 handling is currently disabled");
+    ////        return null;
+    ////    }
+
+    ////    public PartialViewResult Report()
+    ////    {
+    ////        return PartialView();
+    ////    }
+
+    ////    [HttpPost]
+    ////    [ValidateInput(false)]
+    ////    public ViewResult Submit()
+    ////    {
+    ////        //TODO: Transfer Request Values for Email submission
+    ////        return View();
+    ////    }
     }
 }
