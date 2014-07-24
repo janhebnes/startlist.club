cddc3f7bbfea00b4a258cc019fe378a1c5ecb9b3
Jan Hebnes <jan.hebnes@gmail.com>
2014-07-24 15:05:13 +0200

    Cleanup in Routes because we now use constraints

--- FlightJournal.Web/App_Start/RouteConfig.cs
+++ FlightJournal.Web/App_Start/RouteConfig.cs
@@ -7,75 +7,8 @@ namespace FlightJournal.Web{
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            // For allowing reports to be visible from the root addresses /{year} or /{year}/{month} and browsing startlists using {date yyyy-mm-dd}
-            // We must map all Index actions otherwise it will be grabed by the ReportPage routemap
-            routes.MapRoute(
-                "Account", // Route name
-                "Account", // URL with parameters
-                new { controller = "Account", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "Club", // Route name
-                "Club", // URL with parameters
-                new { controller = "Club", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-            routes.MapRoute(
-                "Flight", // Route name
-                "Flight", // URL with parameters
-                new { controller = "Flight", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-            routes.MapRoute(
-                "Home", // Route name
-                "Home", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "Location", // Route name
-                "Location", // URL with parameters
-                new { controller = "Location", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "Manage", // Route name
-                "Manage", // URL with parameters
-                new { controller = "Manage", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "Pilot", // Route name
-                "Pilot", // URL with parameters
-                new { controller = "Pilot", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "PilotStatus", // Route name
-                "PilotStatus", // URL with parameters
-                new { controller = "PilotStatus", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "Plane", // Route name
-                "Plane", // URL with parameters
-                new { controller = "Plane", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "RolesAdmin", // Route name
-                "RolesAdmin", // URL with parameters
-                new { controller = "RolesAdmin", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "StartType", // Route name
-                "StartType", // URL with parameters
-                new { controller = "StartType", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-            routes.MapRoute(
-                "UsersAdmin", // Route name
-                "UsersAdmin", // URL with parameters
-                new { controller = "UsersAdmin", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-            // ---------------------------
-            // Allowing the reports to be visible from the root addresses /{year} or /{year}/{month} and browsing startlists using {date yyyy-mm-dd}
-
+            
+            // Custom club Urls
             routes.MapRoute(
                 "RootClub", // Route name
                 "{club}", // URL with parameters
@@ -83,6 +16,7 @@ namespace FlightJournal.Web{
                 new { club = new ClubRouteConstraint(new ClubValidator()) } 
             );
 
+            // Custom report url /{yyyy} or /{yyyy-MM} or startlist on date {yyyy-MM-dd}
             routes.MapRoute(
                 "ReportingDate", // Route name
                 "{date}", // URL with parameters
@@ -90,13 +24,15 @@ namespace FlightJournal.Web{
                 new { date = new DateRouteConstraint(new DatePathValidator()) }
             );
 
+            // Custom club Urls with custom Date
            routes.MapRoute(
                 "ReportingClubDate", // Route name
                 "{club}/{date}", // URL with parameters
                 new { controller = "Report", action = "Index"},
                 new { club = new ClubRouteConstraint(new ClubValidator()), date = new DateRouteConstraint(new DatePathValidator()) } 
             );
-            
+
+           // Custom club Urls with default behavior
             routes.MapRoute(
                 "DefaultWithClubFlavor", // Route name
                 "{club}/{controller}/{action}/{id}", // URL with parameters
@@ -104,7 +40,7 @@ namespace FlightJournal.Web{
                 new { club = new ClubRouteConstraint(new ClubValidator()) } 
             );
 
-            // Default map
+            // Default behaviour
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
