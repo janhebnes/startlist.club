3283d9a90e285255bccb668b4324575b81b64e20
Jan Hebnes <jan.hebnes@gmail.com>
2014-07-28 22:53:14 +0200

    Introduced demo seed for both databases when an empty database is meet

--- FlightJournal.Web/Models/FlightContext.cs
+++ FlightJournal.Web/Models/FlightContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 
 namespace FlightJournal.Web.Models
@@ -10,8 +11,10 @@ namespace FlightJournal.Web.Models
     {
         public FlightContext() : base("FlightJournal")
         {
+#if DEBUG
             //Database.SetInitializer<FlightContext>(new CreateDatabaseIfNotExists<FlightContext>());
-            //Database.SetInitializer<FlightContext>(new FlightContextInitializer());
+            Database.SetInitializer<FlightContext>(new FlightContextInitializer());
+#endif
         }
 
         public DbSet<Flight> Flights { get; set; }
@@ -100,163 +103,144 @@ namespace FlightJournal.Web.Models
 
         }
 
+        
 #if DEBUG
         /// HACK: Enabled only when working on database structure on the solution in development otherwise it might remove the database data 
-        ////public class FlightContextInitializer : DropCreateDatabaseAlways<FlightContext>
-        ////{
-        ////    protected override void Seed(FlightContext context)
-        ////    {
-        ////        //// --------- Basic user information 
-        ////        ////var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-        ////        ////var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-        ////        ////string name = "Admin";
-        ////        ////string password = "123456";
-        ////        ////string test = "test";
+        public class FlightContextInitializer : CreateDatabaseIfNotExists<FlightContext>
+        {
+            protected override void Seed(FlightContext context)
+            {
+                InitializeDemoFlightsForEF(context);
+                base.Seed(context);
+            }
 
-        ////        //////Create Role Test and User Test
-        ////        ////RoleManager.Create(new IdentityRole(test));
-        ////        ////UserManager.Create(new ApplicationUser() { UserName = test });
+            public static void InitializeDemoFlightsForEF(FlightContext context)
+            {
+                // StartType
+                var start = new StartType() {Name = "Spilstart", ShortName = "S"};
+                context.StartTypes.Add(start);
+                context.StartTypes.Add(new StartType() {Name = "Flyslæb", ShortName = "F"});
+                context.StartTypes.Add(new StartType() {Name = "Selvstart", ShortName = "M"});
+                context.SaveChanges();
 
-        ////        //////Create Role Admin if it does not exist
-        ////        ////if (!RoleManager.RoleExists(name))
-        ////        ////{
-        ////        ////    var roleresult = RoleManager.Create(new IdentityRole(name));
-        ////        ////}
+                // Locations
+                var location = new Location {Name = "Kongsted"};
+                context.Locations.Add(location);
+                context.Locations.Add(new Location() {Name = "Arnborg"});
+                context.SaveChanges();
 
-        ////        //////Create User=Admin with password=123456
-        ////        ////var user = new ApplicationUser();
-        ////        ////user.UserName = name;
-        ////        ////var adminresult = UserManager.Create(user, password);
+                // Clubs
+                var club = new Club() {ClubId = 38, ShortName = "ØSF", Name = "Øst-Sjællands Flyveklub", Location = location};
+                context.Clubs.Add(club);
+                context.SaveChanges();
 
-        ////        //////Add User Admin to Role Admin
-        ////        ////if (adminresult.Succeeded)
-        ////        ////{
-        ////        ////    var result = UserManager.AddToRole(user.Id, name);
-        ////        ////}
+                // Planes
+                var pl2 = new Plane
+                {
+                    CompetitionId = "R2",
+                    Registration = "OY-XMO",
+                    Seats = 2,
+                    DefaultStartType = start,
+                    Engines = 0,
+                    EntryDate = DateTime.Now
+                };
+                context.Planes.Add(pl2);
+                var pla = new Plane
+                {
+                    CompetitionId = "RR",
+                    Registration = "OY-RRX",
+                    Seats = 2,
+                    DefaultStartType = start,
+                    Engines = 1,
+                    EntryDate = DateTime.Now
+                };
+                context.Planes.Add(pla);
+                var pl1 = new Plane
+                {
+                    CompetitionId = "PU",
+                    Registration = "OY-XPU",
+                    Seats = 1,
+                    DefaultStartType = start,
+                    Engines = 0,
+                    EntryDate = DateTime.Now
+                };
+                context.Planes.Add(pl1);
+                context.SaveChanges();
 
-        ////        //// --------- Basic Flight information 
+                // Pilots
+                var pilot = new Pilot {Name = "Jan Hebnes", MemberId = "1241", Club = club};
+                context.Pilots.Add(pilot);
+                context.Pilots.Add(new Pilot {Name = "Søren Sarup", MemberId = "1125", Club = club});
 
-        ////        // Locations
-        ////        var xloc = new Location { Name = "Kongsted" };
-        ////        context.Locations.Add(xloc);
-        ////        context.SaveChanges();
-
-        ////        // Clubs
-        ////        var xclub = new Club() { ClubId = 38, ShortName = "ØSF", Name = "Øst-Sjællands Flyveklub", Location = xloc };
-        ////        context.Clubs.Add(xclub);
-        ////        context.SaveChanges();
-
-        ////        // StartType
-        ////        var xstart = new StartType() { Name = "Spilstart", ShortName = "S" };
-        ////        context.StartTypes.Add(xstart);
-        ////        base.Seed(context);
-                
-        ////        // Locations
-        ////        var loc = new Location { Name = "Kongsted" };
-        ////        context.Locations.Add(loc);
-        ////        context.Locations.Add(new Location() { Name = "Arnborg" });
-        ////        context.SaveChanges();
-
-        ////        // Clubs
-        ////        var club = new Club() { ClubId = 38, ShortName = "ØSF", Name = "Øst-Sjællands Flyveklub", Location = loc };
-        ////        context.Clubs.Add(club);
-        ////        context.SaveChanges();
-
-        ////        // StartType
-        ////        var start = new StartType() { Name = "Spilstart", ShortName = "S" };
-        ////        context.StartTypes.Add(start);
-        ////        context.StartTypes.Add(new StartType() { Name = "Flyslæb", ShortName = "F" });
-        ////        context.StartTypes.Add(new StartType() { Name = "Selvstart", ShortName = "M" });
-        ////        context.SaveChanges();
-
-        ////        context.StartTypes.Add(new StartType() { Name = "Passagerstart", ShortName = "Pass.", Club = club });
-        ////        context.StartTypes.Add(new StartType() { Name = "Gratisstart", ShortName = "Gratis", Club = club });
-        ////        context.SaveChanges();
-
-        ////        // Pilot status
-        ////        var y = new List<string>() { "Ingen", "Prøvemedlem", "Elev", "S-Certifikat", "Instruktør" };
-        ////        y.ForEach(b => context.PilotStatusTypes.Add(new PilotStatusType() { Name = b }));
-        ////        context.SaveChanges();
-
-        ////        // Planes
-        ////        var pl2 = new Plane { CompetitionId = "R2", Registration = "OY-XMO", Seats = 2, DefaultStartType = start, Engines = 0, EntryDate = DateTime.Now };
-        ////        context.Planes.Add(pl2);
-        ////        var pla = new Plane { CompetitionId = "RR", Registration = "OY-RRX", Seats = 2, DefaultStartType = start, Engines = 1, EntryDate = DateTime.Now };
-        ////        context.Planes.Add(pla);
-        ////        var pl1 = new Plane { CompetitionId = "PU", Registration = "OY-XPU", Seats = 1, DefaultStartType = start, Engines = 0, EntryDate = DateTime.Now };
-        ////        context.Planes.Add(pl1);
-        ////        context.SaveChanges();
-
-        ////        // Pilots
-        ////        var pilot = new Pilot { Name = "Jan Hebnes", MemberId = "1241", Club = club };
-        ////        context.Pilots.Add(pilot);
-
-        ////        context.SaveChanges();
-        ////        var s = new List<Flight>
-        ////            {
-        ////                new Flight
-        ////                    {
-        ////                        Departure = DateTime.Now.AddHours(-3),
-        ////                        Landing = DateTime.Now.AddHours(-3).AddMinutes(15),
-        ////                        Plane = pl1,
-        ////                        StartedFrom = loc,
-        ////                        LandedOn = loc, 
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString()
-        ////                    },
-        ////                new Flight
-        ////                    {
-        ////                        Plane = pl2,
-        ////                        StartedFrom = loc,
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString()
-        ////                    },
-        ////                new Flight
-        ////                    {
-        ////                        Departure = DateTime.Now.AddHours(-2),
-        ////                        Plane = pl2,
-        ////                        StartedFrom = loc,
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString(),
-        ////                        Description = "Some more flight description that is very long and complicated and must be handled by the display lists..."
-        ////                    },
-        ////                new Flight
-        ////                    {
-        ////                        Departure = DateTime.Now.AddHours(-1),
-        ////                        Plane = pl2,
-        ////                        StartedFrom = loc,
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString()
-        ////                    },
-        ////                new Flight
-        ////                    {
-        ////                        Departure = DateTime.Now.AddHours(-4),
-        ////                        Plane = pl2,
-        ////                        StartedFrom = loc,
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString(),
-        ////                        Description = "Some flight description..."
-        ////                    },
-        ////                new Flight
-        ////                    {
-        ////                        Departure = DateTime.Now.AddHours(-3).AddMinutes(10),
-        ////                        Plane = pl2,
-        ////                        StartedFrom = loc,
-        ////                        Pilot = pilot,
-        ////                        StartType = start,
-        ////                        LastUpdatedBy = pilot.ToString()
-        ////                    }
-        ////            };
-        ////        s.ForEach(b => context.Flights.Add(b));
-        ////        context.SaveChanges();
-        ////        base.Seed(context);
-        ////    }
-        ////}
+                context.SaveChanges();
+                var s = new List<Flight>
+                {
+                    new Flight
+                    {
+                        Departure = DateTime.Now.AddHours(-3),
+                        Landing = DateTime.Now.AddHours(-3).AddMinutes(15),
+                        Plane = pl1,
+                        StartedFrom = location,
+                        LandedOn = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        Description = "Demo flight",
+                        LastUpdatedBy = pilot.ToString()
+                    },
+                    new Flight
+                    {
+                        Plane = pl2,
+                        StartedFrom = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        Description = "Demo flight",
+                        LastUpdatedBy = pilot.ToString()
+                    },
+                    new Flight
+                    {
+                        Departure = DateTime.Now.AddHours(-2),
+                        Plane = pl2,
+                        StartedFrom = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        LastUpdatedBy = pilot.ToString(),
+                        Description = "Demo flight"
+                    },
+                    new Flight
+                    {
+                        Departure = DateTime.Now.AddHours(-1),
+                        Plane = pl2,
+                        StartedFrom = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        Description = "Demo flight",
+                        LastUpdatedBy = pilot.ToString()
+                    },
+                    new Flight
+                    {
+                        Departure = DateTime.Now.AddHours(-4),
+                        Plane = pl2,
+                        StartedFrom = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        LastUpdatedBy = pilot.ToString(),
+                        Description = "Demo flight"
+                    },
+                    new Flight
+                    {
+                        Departure = DateTime.Now.AddHours(-3).AddMinutes(10),
+                        Plane = pl2,
+                        StartedFrom = location,
+                        Pilot = pilot,
+                        StartType = start,
+                        Description = "Demo flight",
+                        LastUpdatedBy = pilot.ToString()
+                    }
+                };
+                s.ForEach(b => context.Flights.Add(b));
+                context.SaveChanges();
+            }
+        }
 #endif
     }
 }
--- FlightJournal.Web/Models/IdentityModels.cs
+++ FlightJournal.Web/Models/IdentityModels.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace FlightJournal.Web.Models 
 {
@@ -23,9 +25,9 @@ namespace FlightJournal.Web.Models
 
         static ApplicationDbContext() {
 #if DEBUG
-            ////// Set the database intializer which is run once during application start
-            ////// This seeds the database with admin user credentials and admin role
-            ////Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
 #endif
         }
 
@@ -33,4 +35,49 @@ namespace FlightJournal.Web.Models
             return new ApplicationDbContext();
         }
     }
+
+#if DEBUG
+    /// HACK: Enabled only when working on database structure on the solution in development otherwise it might remove the database data 
+    public class ApplicationDbInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            InitializeMembershipsForEF(context);
+            base.Seed(context);
+        }
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeMembershipsForEF(ApplicationDbContext db)
+        {
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+            const string name = "admin@admin.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+#endif
 }
