249c12087139bb0bbf5a05c8c1f6bb2ed3f6f18f
Jan Hebnes <jan.hebnes@gmail.com>
2014-08-01 22:34:28 +0200

    Added UnitTests for Service Providers

---
+++ FlightJournal.Tests/ServiceProviders/SendGridTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using FlightJournal.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SendGrid;
+
+namespace FlightJournal.Tests.ServiceProviders
+{
+    [TestClass]
+    
+    public class SendGridTest
+    {
+        [TestMethod]
+        [TestCategory("ServiceProviders")]
+        public void SendGrid_Send_Mail_with_API()
+        {
+            var settings = ConfigurationManager.GetSection("serviceCredentials") as ServiceCredentialsConfigurationSection;
+            if (settings == null)
+                throw new ConfigurationErrorsException("Missing ServiceCredentials section");
+
+            if (!string.IsNullOrWhiteSpace(settings.TwilioAccountSid)
+                && !string.IsNullOrWhiteSpace(settings.TwilioAuthToken)
+                && !string.IsNullOrWhiteSpace(settings.TwilioFromNumber))
+            {
+                // Create the email object first, then add the properties.
+                SendGridMessage myMessage = new SendGridMessage();
+                myMessage.AddTo("jan.hebnes@gmail.com");
+                myMessage.From = new MailAddress(settings.SendGridFromEmail, settings.SendGridFromName);
+                myMessage.Subject = "SendGrid Testing account setup for startlist.club";
+                myMessage.Text = "Hello World";
+
+                // Create credentials, specifying your user name and password.
+                var credentials = new NetworkCredential(settings.SendGridUserName, settings.SendGridPassword);
+
+                // Create an Web transport for sending email.
+                var transportWeb = new SendGrid.Web(credentials);
+
+                // Send the email.
+                transportWeb.Deliver(myMessage);
+
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.Fail("Configuration not set");
+            }
+        }
+    }
+}
---
+++ FlightJournal.Tests/ServiceProviders/TwilioTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using FlightJournal.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twilio;
+
+namespace FlightJournal.Tests.ServiceProviders
+{
+    [TestClass]
+    public class TwilioTest
+    {
+        [TestMethod]
+        [TestCategory("ServiceProviders")]
+        public void Twilio_Send_SMS_with_API()
+        {
+            var settings = ConfigurationManager.GetSection("serviceCredentials") as ServiceCredentialsConfigurationSection;
+            if (settings == null)
+                throw new ConfigurationErrorsException("Missing ServiceCredentials section");
+
+            if (!string.IsNullOrWhiteSpace(settings.TwilioAccountSid)
+                && !string.IsNullOrWhiteSpace(settings.TwilioAuthToken)
+                && !string.IsNullOrWhiteSpace(settings.TwilioFromNumber))
+            {
+
+                // Find your Account Sid and Auth Token at twilio.com/user/account 
+                var twilio = new TwilioRestClient(settings.TwilioAccountSid, settings.TwilioAuthToken);
+                var smsmessage = twilio.SendMessage(settings.TwilioFromNumber, "+4524250682", "Twilio Testing account setup for startlist.club");
+
+                Assert.IsNull(smsmessage.ErrorMessage);
+            }
+            else
+            {
+                Assert.Fail("Configuration not set");
+            }
+        }
+    }
+}
