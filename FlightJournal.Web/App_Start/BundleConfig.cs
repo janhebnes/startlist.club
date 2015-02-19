dc0f77c66b1d63399a17a29bee7d9c0773e96b8c
Jan Hebnes <jan.hebnes@gmail.com>
2015-02-19 22:59:00 +0100

    Revert "removed jquery ui theme css not used in the project"
    This reverts commit 1a0cb88e21b5fa678c51b22028e69b15a5dfa922.

--- FlightJournal.Web/App_Start/BundleConfig.cs
+++ FlightJournal.Web/App_Start/BundleConfig.cs
@@ -29,20 +29,19 @@ namespace FlightJournal.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/site.css",
                       "~/Content/themes/base/core.css",
+                      "~/Content/themes/base/resizable.css",
+                      "~/Content/themes/base/selectable.css",
+                      "~/Content/themes/base/accordion.css",
                       "~/Content/themes/base/autocomplete.css",
+                      "~/Content/themes/base/button.css",
+                      "~/Content/themes/base/dialog.css",
+                      "~/Content/themes/base/slider.css",
+                      "~/Content/themes/base/tabs.css",
                       "~/Content/themes/base/datepicker.css",
-                      "~/Content/themes/base/theme.css",
-                      "~/Content/site.css"));
-                      //"~/Content/themes/base/resizable.css",
-                      //"~/Content/themes/base/selectable.css",
-                      //"~/Content/themes/base/accordion.css",
-                      //"~/Content/themes/base/button.css",
-                      //"~/Content/themes/base/dialog.css",
-                      //"~/Content/themes/base/slider.css",
-                      //"~/Content/themes/base/tabs.css",                      
-                      //"~/Content/themes/base/progressbar.css",
-                      //"~/Content/themes/base/theme.css"
+                      "~/Content/themes/base/progressbar.css",
+                      "~/Content/themes/base/theme.css"));
             
             //// When using deployment and not running in debug mode it is expected that the bundling automatically takes place
             //// BundleTable.EnableOptimizations = true;
