1762a0443e2b3c7a623a10738a56376f1d680649
Jan Hebnes <jan.hebnes@gmail.com>
2015-03-12 22:01:53 +0100

    created applicationDb Seed for demo accounts

--- FlightJournal.Web/Migrations/ApplicationDbContext/Configuration.cs
+++ FlightJournal.Web/Migrations/ApplicationDbContext/Configuration.cs
@@ -1,3 +1,4 @@
+using System.Security;
 using System.Web;
 using FlightJournal.Web.Models;
 using Microsoft.AspNet.Identity;
@@ -42,18 +43,43 @@ namespace FlightJournal.Web.Migrations.ApplicationDbContext
             //
         }
 
-        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        //Create demo accounts and their roles
         public static void InitializeDemoMemberships(Models.ApplicationDbContext db)
         {
-            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-            const string name = "admin@admin.com";
-            const string password = "Admin@123456";
-            const string roleName = "Admin";
+            foreach (var demoUsers in FlightJournal.Web.Configuration.Demo.GetDemoMembershipTemplates)
+            {
+                GenerateRoleAndDemoUser(db, demoUsers.RoleName, demoUsers.Name, demoUsers.Password, demoUsers.PhoneNumber);
+            }
+        }
 
-            //Create Role Admin if it does not exist
+        private static void GenerateRoleAndDemoUser(Models.ApplicationDbContext db, string roleName, string name, string password, string phoneNumber)
+        {
+            // remove this, because the OWIN context does not yet exist:
+            ////var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            ////var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+            // and replace it with:
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(db));
+            var roleManager = new ApplicationRoleManager(new RoleStore<IdentityRole>(db));
+
+            // because the context is created locally it must be configured with basic information to function
+            userManager.UserValidator = new UserValidator<ApplicationUser>(userManager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            // Configure validation logic for passwords
+            userManager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 5,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+
+            //Create Role if it does not exist
             var role = roleManager.FindByName(roleName);
-            if (role == null)
+            if (role == null && !string.IsNullOrEmpty(roleName))
             {
                 role = new IdentityRole(roleName);
                 var roleresult = roleManager.Create(role);
@@ -62,15 +88,27 @@ namespace FlightJournal.Web.Migrations.ApplicationDbContext
             var user = userManager.FindByName(name);
             if (user == null)
             {
-                // Allowing match to be made on Pilot Binding
-                user = new ApplicationUser { UserName = name, Email = name, PhoneNumber = "+4524250682", PhoneNumberConfirmed = true };
+                // Allowing match on Mobil nr to be made on Pilot Binding
+                user = new ApplicationUser
+                {
+                    UserName = name,
+                    Email = name,
+                    PhoneNumber = phoneNumber,
+                    PhoneNumberConfirmed = true
+                };
                 var result = userManager.Create(user, password);
+                if (!result.Succeeded)
+                {
+                    throw new SecurityException(string.Format("Failed to generate user {0} for ApplocationDbContext {1}", user, result.Errors.FirstOrDefault()));
+                }
                 result = userManager.SetLockoutEnabled(user.Id, false);
             }
 
-            // Add user admin to Role Admin if not already added
+            // Add user to Role if not already added
             var rolesForUser = userManager.GetRoles(user.Id);
-            if (!rolesForUser.Contains(role.Name))
+            if (role != null 
+                && !string.IsNullOrEmpty(roleName) 
+                && !rolesForUser.Contains(role.Name))
             {
                 var result = userManager.AddToRole(user.Id, role.Name);
             }
