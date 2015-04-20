ef86e9524d2a30bde505a8566492e20b86089dee
Jan Hebnes <jan.hebnes@gmail.com>
2015-04-20 12:00:41 +0200

    Added timespan Extension to format time as total Hours and minutes

---
+++ FlightJournal.Tests/Extensions/TimeSpanExtensionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FlightJournal.Web.Extensions;
+
+namespace FlightJournal.Tests.Extensions
+{
+    /// <summary>
+    /// Validates that the TimeSpanExtensions function as expected
+    /// </summary>
+    [TestClass]
+    public class TimeSpanExtensionTest
+    {
+        [TestMethod]
+        public void FormattingBelowFullDayValues()
+        {
+            var time = new TimeSpan(0,10,20,0,0);
+            string formatted = time.TotalHoursWithMinutesAsDecimal();
+            Assert.AreEqual(formatted, "10:20");
+        }
+
+        [TestMethod]
+        public void FormattingSmallValues()
+        {
+            var time = new TimeSpan(0, 26, 20, 0, 0);
+            string formatted = time.TotalHoursWithMinutesAsDecimal();
+            Assert.AreEqual(formatted, "26:20");
+        }
+
+        [TestMethod]
+        public void FormattingAboveFullDayValues()
+        {
+            var time = new TimeSpan(2, 10, 20, 0, 0);
+            string formatted = time.TotalHoursWithMinutesAsDecimal();
+            Assert.AreEqual(formatted, "58:20");
+        }
+    }
+}
---
+++ FlightJournal.Web/Extensions/TimeSpanExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightJournal.Web.Extensions
+{
+    public static class TimeSpanExtension
+    {
+        /// <summary>
+        /// Format the TimeSpan as TotalHours : Minutes 
+        /// </summary>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
+        public static string TotalHoursWithMinutesAsDecimal(this TimeSpan timeSpan)
+        {
+            if (timeSpan.TotalMinutes > 0)
+            {
+                return string.Format("{0}:{1}"
+                    , timeSpan.TotalHours.ToString("#00.0").Substring(0, timeSpan.TotalHours.ToString("#00.0", CultureInfo.InvariantCulture).IndexOf(".", StringComparison.InvariantCulture))
+                    , timeSpan.Minutes.ToString("#00"));
+            }
+
+            return string.Empty;
+        }
+    }
+}
