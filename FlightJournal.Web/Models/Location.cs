dd66fa331763af3ef1390bcffa794d65184e1661
Jan Hebnes <jan.hebnes@gmail.com>
2015-04-26 20:15:05 +0200

    Added Icao and country to entity location

--- FlightJournal.Web/Models/Location.cs
+++ FlightJournal.Web/Models/Location.cs
@@ -1,10 +1,17 @@
-﻿namespace FlightJournal.Web.Models
+﻿using System;
+
+namespace FlightJournal.Web.Models
 {
     public class Location
     {
         public int LocationId { get; set; }
-        public string Name {get; set; }
-
+        public string Name { get; set; }
+        public string ICAO { get; set; }
+        public string Country { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdatedTimestamp { get; set; }
+        public string LastUpdatedBy { get; set; }
         public override string ToString()
         {
             return this.Name;
