196801f7b476b6195fde22f9f97b527e3407af3e
Jan Hebnes <jan.hebnes@gmail.com>
2015-05-27 23:51:14 +0200

    Introduced FairlyLocal source code as base
    http://www.fairtutor.com/fairlylocal/

---
+++ FlightJournal.Web/Translations/Internationalization.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace FlightJournal.Web.Translations
+{
+    public static class Internationalization
+    {
+        public static class Settings
+        {
+            /// <summary>
+            /// The language that MsgIDs are given in (generally the language the project is being developed in).
+            /// </summary>
+            public static string WorkingLanguage = "en";
+
+            /// <summary>
+            /// This is the base path under which localizations will 
+            /// be stored.
+            /// </summary>
+            public static string BasePath = "~/Translations/";
+
+            /// <summary>
+            /// The session key that will be used to store a user's language preference
+            /// </summary>
+            public static string LanguageCodeSessionKey = "LanguageCode";
+
+            /// <summary>
+            /// FOR TESTING ONLY!  If true, all calls to GetText will return an empty string.
+            /// this is useful when searching for any strings that might not be flagged for localization.
+            /// </summary>
+            public static bool HideAllLocalizedText;
+        }
+
+        private static string _basePathAbsolute;
+        public static Dictionary<string, Localization> Localizations;
+
+        static Internationalization()
+        {
+            Localizations = new Dictionary<string, Localization>();
+            _basePathAbsolute = HttpContext.Current.Server.MapPath(Settings.BasePath).ToLower();
+
+            if (!Directory.Exists(_basePathAbsolute))
+            {
+                Directory.CreateDirectory(_basePathAbsolute);
+            }
+
+            foreach (string dir in Directory.GetDirectories(_basePathAbsolute))
+            {
+                string lcPath = Path.Combine(dir, "LC_MESSAGES");
+                if (!Directory.Exists(lcPath))
+                {
+                    continue;
+                }
+
+                Localization l = new Localization();
+                foreach (string filename in Directory.GetFiles(lcPath, "*.po"))
+                {
+                    l.LoadFromFile(filename);
+                }
+
+                if (l.Messages.Count > 0)
+                {
+                    Localizations.Add(dir.Replace(_basePathAbsolute, "").ToLower(), l);
+                }
+            }
+
+            if (!Localizations.ContainsKey(Settings.WorkingLanguage))
+            {
+                Localizations.Add(Settings.WorkingLanguage, new Localization());
+            }
+        }
+
+        #region public methods
+
+        /// <summary>
+        /// Get a translated version of the supplied text message.
+        /// </summary>
+        /// <param name="msgID">Text to be translated</param>
+        /// <param name="languageCode">Language to translate into</param>
+        /// <returns></returns>
+        public static string GetText(string msgID, string languageCode)
+        {
+            if (Settings.HideAllLocalizedText)
+            {
+                return "";
+            }
+
+            if (!Localizations.ContainsKey(languageCode.ToLower()))
+            {
+                languageCode = Settings.WorkingLanguage;
+            }
+
+            return Localizations[languageCode.ToLower()].GetMessage(msgID);
+        }
+
+        /// <summary>
+        /// Spins through the language preferences in the supplied HttpRequest,
+        /// returning the first complete or partial match on a loaded language.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
+        public static string GetBestLanguage(HttpRequest request, string fallback)
+        {
+            if (request.UserLanguages == null || request.UserLanguages.Length == 0)
+            {
+                return Settings.WorkingLanguage;
+            }
+
+            foreach (string lang in request.UserLanguages)
+            {
+                if (Localizations.ContainsKey(lang.ToLower()))
+                {
+                    return lang;
+                }
+
+                string fragment = lang.ToLower().Split('-')[0];
+                if (Localizations.ContainsKey(fragment))
+                {
+                    return fragment;
+                }
+
+            }
+
+            return fallback;
+        }
+
+        #endregion
+
+    }
+}
---
+++ FlightJournal.Web/Translations/Message.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightJournal.Web.Translations
+{
+    /// <summary>
+    /// A piece of text that may have been translated into another language.
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// The original text of the message, as scraped from source code.
+        /// </summary>
+        public string MsgID { get; set; }
+
+        /// <summary>
+        /// The translated text, or an empty string if the message has not yet been translated.
+        /// </summary>
+        public string MsgStr { get; set; }
+
+        /// <summary>
+        /// A list of comments found with this message in its .po file.
+        /// Will only be populated if the loadComments flag is set while loading the Localization.
+        /// </summary>
+        public List<string> Comments { get; set; }
+
+        /// <summary>
+        /// A list of filename:linenumber pairs representing where this message
+        /// was found in the source code.
+        /// Will only be populated if the loadComments flag is set while loading the Localization.
+        /// </summary>
+        public List<string> Contexts { get; set; }
+
+        // A temporary short ID that can be used for lookups.
+        // (but don't store it since it will change depending on the order in which messages are loaded)
+        public int AutoID { get; set; }
+
+
+        private static int _nextAutoID;
+
+        /// <summary>
+        /// Create a new, empty Message
+        /// </summary>
+        public Message()
+        {
+            AutoID = _nextAutoID++;
+            MsgID = "";
+            MsgStr = "";
+            Contexts = new List<string>();
+            Comments = new List<string>();
+        }
+
+        /// <summary>
+        /// Create a new Message, setting the msgID to the supplied value
+        /// </summary>
+        /// <param name="msgID"></param>
+        public Message(string msgID)
+            : this()
+        {
+            MsgID = msgID;
+        }
+
+        /// <summary>
+        /// Create a new Message, setting the supplied values
+        /// </summary>
+        /// <param name="msgID"></param>
+        /// <param name="msgStr"></param>
+        public Message(string msgID, string msgStr)
+            : this()
+        {
+            MsgID = msgID;
+            MsgStr = msgStr;
+        }
+
+        /// <summary>
+        /// Create a new Message, setting the supplied values
+        /// </summary>
+        /// <param name="msgID"></param>
+        /// <param name="msgStr"></param>
+        /// <param name="firstContext"></param>
+        public Message(string msgID, string msgStr, string firstContext)
+            : this()
+        {
+            MsgID = msgID;
+            MsgStr = msgStr;
+            Contexts.Add(firstContext);
+        }
+
+        /// <summary>
+        /// Return the translated version of this message. 
+        /// If no translation is available, return the message itself
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return (String.IsNullOrEmpty(MsgStr)) ? MsgID : MsgStr;
+        }
+
+        /// <summary>
+        /// Return this message, formatted as it would appear in a .po file
+        /// </summary>
+        /// <returns></returns>
+        public string ToPOBlock()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string comment in Comments)
+            {
+                builder.AppendLine(comment);
+            }
+
+            return String.Format(@"
+#: {0}
+msgid ""{1}""
+msgstr ""{2}""
+"
+                , builder
+                , MsgID
+                , MsgStr);
+        }
+    }
+}
