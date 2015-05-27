a3cbc8d7794ffeb2a3dc60eef6b24fea11a1f1a4
Jan Hebnes <jan.hebnes@gmail.com>
2015-05-27 23:32:36 +0200

    Added Batch using Gettext.Tools.0.19.4\tools\xgettext.exe
    and merging existing po files with latest pot file - uses an input file
    with relative paths for not poluting git with absolute paths in po file
    comments

---
+++ Translation.cmd
@@ -0,0 +1,40 @@
+@echo off 
+rem --------------------------
+rem - REQUIRED - INSTALL NuGet packages\Gettext.Tools.0.19.4
+rem -------------------------- 
+
+rem 1) Generate inputfiles list used by xgettext
+
+rem MODEL A (absolute path)
+rem dir FlightJournal.Web\*.cs /S /B > translation-inputfiles.txt
+rem dir FlightJournal.Web\*.cshtml /S /B >> translation-inputfiles.txt
+
+rem MODEL B Specific folders - removing obj and the like (absolute path) 
+rem dir FlightJournal.Web\Controllers\*.cs /S /B > translation-inputfiles.txt
+rem dir FlightJournal.Web\Models\*.cs /S /B > translation-inputfiles.txt
+rem dir FlightJournal.Web\Views\*.cs /S /B > translation-inputfiles.txt
+rem dir FlightJournal.Web\Views\*.cshtml /S /B >> translation-inputfiles.txt
+
+rem MODEL C Relative paths - but with obj folders and package folders (anyone fresh for a fix ?)
+@echo on>translation-inputfiles.txt
+@echo off 
+setlocal EnableDelayedExpansion
+for /L %%n in (1 1 500) do if "!__cd__:~%%n,1!" neq "" set /a "len=%%n+1"
+setlocal DisableDelayedExpansion
+for /r . %%g in (*.cs,*.cshtml) do (
+  set "absPath=%%g"
+  setlocal EnableDelayedExpansion
+  set "relPath=!absPath:~%len%!"
+  echo(!relPath! >> translation-inputfiles.txt
+  endlocal
+)
+
+rem Create a new .pot from source, place it in the Language folder, and merge with the existing .po file
+packages\Gettext.Tools.0.19.4\tools\xgettext.exe -k -k_ --from-code=UTF-8 -L C# --omit-header -o FlightJournal.Web\Translations\messages.pot -f translation-inputfiles.txt
+packages\Gettext.Tools.0.19.4\tools\msgmerge.exe --backup=none -U FlightJournal.Web\Translations\en\LC_MESSAGES\messages.po FlightJournal.Web\Translations\messages.pot rem EN must always match pot file
+packages\Gettext.Tools.0.19.4\tools\msgmerge.exe --backup=none -U FlightJournal.Web\Translations\da\LC_MESSAGES\messages.po FlightJournal.Web\Translations\messages.pot
+
+rem begin new language by copying 
+rem 		FlightJournal.Web\Translations\messages.pot 
+rem 	to 	FlightJournal.Web\Translations\LANG\LC_MESSAGES\messages.po
+rem 	and add line for msgmerge.exe for merging changes to pot into existing LANG messages.po
