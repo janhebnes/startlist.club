ef0dad095e182c9fbb32a2af07d26f88b90b6ae3
Jan Hebnes <jan.hebnes@gmail.com>
2016-03-29 23:16:43 +0200

    Added nuget OGN.FlightLog.Client
    Notice the EntityFramework is upgraded one minor in the process

--- FlightJournal.Web/App_Start/Startup.OpenGliderNetworkFlightLogMonitor.cs
+++ FlightJournal.Web/App_Start/Startup.OpenGliderNetworkFlightLogMonitor.cs
@@ -11,28 +11,31 @@ namespace FlightJournal.Web
     {
         public void ConfigureOpenGliderNetworkFlightLogMonitor(IAppBuilder app)
         {
+
+
+
             // TODO: Do we create a new Db Context for Ogn log data .. ?<s
             //app.CreatePerOwinContext(ApplicationDbContext.Create);
 
             //http://live.glidernet.org/flightlog/index.php?a=EHDL&s=QFE&u=M&z=2&p=&d=30052015&j
             //http://stackoverflow.com/questions/32401704/appending-json-data-to-listview-c-sharp
 
-    //        WebClient client = new WebClient();
-    //        string json = client.DownloadString("http://live.glidernet.org/flightlog/index.php?a=EHDL&s=QFE&u=M&z=2&p=&d=30052015&j");
+            //        WebClient client = new WebClient();
+            //        string json = client.DownloadString("http://live.glidernet.org/flightlog/index.php?a=EHDL&s=QFE&u=M&z=2&p=&d=30052015&j");
 
-    //        JObject data = JObject.Parse(json);
+            //        JObject data = JObject.Parse(json);
 
-    //        // create an array of ListViewItems from the JSON
-    //        var items = data["flights"]
-    //            .Children<JObject>()
-    //            .Select(jo => new ListViewItem(new string[] 
-    //{
-    //    (string)jo["glider"],
-    //    (string)jo["takeoff"],
-    //    (string)jo["glider_landing"],
-    //    (string)jo["glider_time"]
-    //}))
-    //            .ToArray();
+            //        // create an array of ListViewItems from the JSON
+            //        var items = data["flights"]
+            //            .Children<JObject>()
+            //            .Select(jo => new ListViewItem(new string[] 
+            //{
+            //    (string)jo["glider"],
+            //    (string)jo["takeoff"],
+            //    (string)jo["glider_landing"],
+            //    (string)jo["glider_time"]
+            //}))
+            //            .ToArray();
 
             // NOTE: Should we owin the FlightContext  see the use of context.Get<ApplicationDbContext>() that allows for a single instance to be used all over... 
         }
