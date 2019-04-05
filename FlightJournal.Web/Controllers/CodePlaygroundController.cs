29043cd8bddafbcc45ad73ecc62999a8f4645a2c
Jan Hebnes <jan.hebnes@gmail.com>
2019-04-06 00:21:58 +0200

    Generated a playground controller for storing trials with Vue and Ui update workshops

---
+++ FlightJournal.Web/Controllers/CodePlaygroundController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace FlightJournal.Web.Controllers
+{
+    [Authorize]
+    [RoutePrefix("Book-Reviews")]
+    public class CodePlaygroundController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Administration()
+        {
+            return View();
+        }
+        
+        public ActionResult License()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult TableExpand()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult TableExpandPartial()
+        {
+            return this.PartialView();
+        }
+
+    }
+}
