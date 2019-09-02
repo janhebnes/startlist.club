18554eceb825e9c9223186ddc1e80cba47467859
Jan Hebnes <jan.hebnes@gmail.com>
2019-09-02 22:21:53 +0200

    StartType is now being localized

--- FlightJournal.Web/Models/StartType.cs
+++ FlightJournal.Web/Models/StartType.cs
@@ -11,6 +11,31 @@ namespace FlightJournal.Web.Models
         public int StartTypeId { get; set; }
         [LocalizedDisplayName("Shortname")]
         public string ShortName { get; set; }
+
+        public string LocalizedShortName
+        {
+            // BGA: Launch Type – W (Winch); B (Bungee); A (Aerotow); S (Self Launch); TMG (Touring Motor Glider)
+            // Denmark: Start typer - S (Spilstart); F (Flyslæb); M (Selvstart)
+            // Norge: Startmetodene - V (Vinsj); F (Flyslep); M (Selvstart);
+            get
+            {
+                if (ShortName == "S" && Name == "Spilstart")
+                    return ShortHand(_("W (Winch)"));
+                else if (ShortName == "F" && Name == "Flyslæb")
+                    return ShortHand(_("A (Aerotow)"));
+                else if (ShortName == "M" && Name == "Selvstart")
+                    return ShortHand(_("S (Self Launch)"));
+                else
+                    return ShortName;
+
+                // Take the first short hand... 
+                string ShortHand(string localizedDisplayName)
+                {
+                    return localizedDisplayName.Split(' ')[0];
+                }
+            }
+        }
+
         [LocalizedDisplayName("Name")]
         public string Name { get; set; }
 
@@ -18,9 +43,32 @@ namespace FlightJournal.Web.Models
         [LocalizedDisplayName("Club")]
         public virtual Club Club { get; set; }
 
+        public string LocalizedDisplayName
+        {
+            // BGA: Launch Type – W (Winch); B (Bungee); A (Aerotow); S (Self Launch); TMG (Touring Motor Glider)
+            // Denmark: Start typer - S (Spilstart); F (Flyslæb); M (Selvstart)
+            // Norge: Startmetodene - V (Vinsj); F (Flyslep); M (Selvstart);
+            get
+            {
+                if (ShortName == "S" && Name == "Spilstart")
+                    return _("W (Winch)");
+                else if (ShortName == "F" && Name == "Flyslæb")
+                    return _("A (Aerotow)");
+                else if (ShortName == "M" && Name == "Selvstart")
+                    return _("S (Self Launch)");
+                else
+                    return Name;
+            }
+        }
+
         public override string ToString()
         {
             return this.Name;
+        }
+
+        private string _(string resourceId)
+        {
+            return Internationalization.GetText(resourceId, Internationalization.LanguageCode);
         }
     }
 }
