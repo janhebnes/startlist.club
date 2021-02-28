467ee2fa0247ea4a13a835334531db9b905c22d7
Per <per@st41.dk>
2021-02-28 13:53:01 +0100

    Misc fixes

--- FlightJournal.Web/Controllers/TrainingProgramAdminController.cs
+++ FlightJournal.Web/Controllers/TrainingProgramAdminController.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using FlightJournal.Web.Models;
@@ -31,7 +32,7 @@ namespace FlightJournal.Web.Controllers
 
         public ActionResult Create()
         {
-            return View();
+            return View(new Training2Program());
         }
 
         [HttpPost]
@@ -91,7 +92,22 @@ namespace FlightJournal.Web.Controllers
             base.Dispose(disposing);
         }
 
+        public ActionResult Export(int id)
+        {
+            var item = db.TrainingPrograms.Find(id);
 
+            var sb = new StringBuilder();
+            var serializer = new JsonSerializer { NullValueHandling = NullValueHandling.Ignore };
+
+            using (var sw = new StringWriter(sb))
+            using (var writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
+            {
+                serializer.Serialize(writer, item);
+            }
+
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString()), System.Net.Mime.MediaTypeNames.Application.Octet, $"{item.Name}.json");
+        }
 
         [HttpPost]
         public ActionResult Import(HttpPostedFileBase file)
