344864acb0fd318d3666cd79c987ae74d8892ff3
Tomas Kulik <tomas.kulik@icloud.com>
2021-03-03 19:23:50 +0100

    Added SignalR and scaffolding + mapped the Hub

---
+++ FlightJournal.Web/Hubs/FlightHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace FlightJournal.Web.Hubs
+{
+    public class FlightHub : Hub
+    {
+        public void InformClientsStaleData()
+        {
+            Clients.All.staleData();
+        }
+    }
+}
--- FlightJournal.Web/Startup.cs
+++ FlightJournal.Web/Startup.cs
@@ -8,6 +8,7 @@ namespace FlightJournal.Web
         {
             ConfigureAuth(app);
             ConfigureOpenGliderNetworkFlightLogMonitor(app);
+            app.MapSignalR();
         }
     }
 }
