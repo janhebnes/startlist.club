dd0f15fa3599a408897fc8b68bdebda481606e10
Jan Hebnes <jan@hebnes.dk>
2021-11-16 00:03:03 +0100

    Flights extended with index IX_Flights_Deleted_PilotBackseatId_StartTypeId as Per Recommendation in #199

---
+++ FlightJournal.Web/Migrations/FlightContext/202111152258080_Flights extended with index IX_Flights_Deleted_PilotBackseatId_StartTypeId.cs
@@ -0,0 +1,18 @@
+﻿namespace FlightJournal.Web.Migrations.FlightContext
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FlightsextendedwithindexIX_Flights_Deleted_PilotBackseatId_StartTypeId : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE NONCLUSTERED INDEX [IX_Flights_Deleted_PilotBackseatId_StartTypeId] ON[dbo].[Flights]([Deleted], [PilotBackseatId], [StartTypeId]) INCLUDE([BetalerId], [Date], [Departure], [Description], [FlightCost], [HasTrainingData], [LandedOnId], [Landing], [LandingCount], [LastUpdated], [LastUpdatedBy], [PilotId], [PlaneId], [RecordKey], [StartCost], [StartedFromId], [TachoCost], [TachoDeparture], [TachoLanding], [TaskDistance]) ");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Flights", "IX_Flights_Deleted_PilotBackseatId_StartTypeId");
+        }
+    }
+}
