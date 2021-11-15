e2428a10c9b66fc832b2d1d5913de609d60f80ae
Jan Hebnes <jan@hebnes.dk>
2021-11-16 00:09:56 +0100

    AppliedExercises extended with index IX_AppliedExercises_FlightId as Per Recommendation in #198

---
+++ FlightJournal.Web/Migrations/FlightContext/202111152304567_AppliedExercises extended with index IX_AppliedExercises_FlightId.cs
@@ -0,0 +1,18 @@
+﻿namespace FlightJournal.Web.Migrations.FlightContext
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AppliedExercisesextendedwithindexIX_AppliedExercises_FlightId : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE NONCLUSTERED INDEX [IX_AppliedExercises_FlightId] ON [dbo].[AppliedExercises] ([FlightId])");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.AppliedExercises", "IX_AppliedExercises_FlightId");
+        }
+    }
+}
