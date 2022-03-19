0482301a128d35ac81d030bbfea527eba592be1e
Per <per@st41.dk>
2022-03-19 13:49:03 +0100

    #207 show training barometer status in grid

---
+++ FlightJournal.Web/Models/GridViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FlightJournal.Web.Models
+{
+    public class GridViewModel
+    {
+        public IEnumerable<Flight> Flights { get; }
+        public Dictionary<int, TrainingBarometerViewModel> TrainingBarometers { get; }
+
+        public GridViewModel(IEnumerable<Flight> flights,
+            Dictionary<int, TrainingBarometerViewModel> trainingBarometers)
+        {
+            Flights = flights;
+            TrainingBarometers = trainingBarometers;
+        }
+    }
+}
