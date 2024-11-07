07e1cdbc477df4e72dcf9e690206d116e1da4361
Jan Hebnes <jan.hebnes@gmail.com>
2024-11-08 00:01:01 +0100

    First round of nuget upgrades that have been checked out by manuel testing and validation

--- FlightJournal.Tests/ServiceProviders/TwilioTest.cs
+++ FlightJournal.Tests/ServiceProviders/TwilioTest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using FlightJournal.Web.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 //using Twilio;
 
 namespace FlightJournal.Tests.ServiceProviders
@@ -14,7 +16,6 @@ namespace FlightJournal.Tests.ServiceProviders
         public void Twilio_Send_SMS_with_API()
         {
             Assert.Inconclusive("Not active on normal test cycle");
-
             //var settings = ConfigurationManager.GetSection("serviceCredentials") as ServiceCredentialsConfigurationSection;
             //if (settings == null)
             //    throw new ConfigurationErrorsException("Missing ServiceCredentials section");
@@ -25,8 +26,13 @@ namespace FlightJournal.Tests.ServiceProviders
             //{
 
             //    // Find your Account Sid and Auth Token at twilio.com/user/account 
-            //    var twilio = new TwilioRestClient(settings.TwilioAccountSid, settings.TwilioAuthToken);
-            //    var smsmessage = twilio.SendMessage(settings.TwilioFromNumber, "+4524250682", "Twilio Testing account setup for startlist.club");
+            //    TwilioClient.Init(Settings.TwilioAccountSid, Settings.TwilioAuthToken);
+
+            //    var smsmessage = MessageResource.Create(
+            //        "+4524250682",
+            //        from: Settings.TwilioFromNumber,
+            //        body: "Twilio Testing account setup for startlist.club"
+            //    );
 
             //    Assert.IsNotNull(smsmessage.Sid);
             //    Assert.IsNull(smsmessage.ErrorMessage);
