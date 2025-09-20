34cf3e0579cc85596f2a293a1a96a4c2ed4b6414
Jan Hebnes <jan.hebnes@gmail.com>
2025-09-20 23:21:52 +0200

    adding brackets to namespace level in PilotDetailsViewModel to repair appveyor

--- FlightJournal.Web/Models/PilotDetailsViewModel.cs
+++ FlightJournal.Web/Models/PilotDetailsViewModel.cs
@@ -3,86 +3,87 @@ using System.ComponentModel;
 using System.Linq;
 using FlightJournal.Web.Models.Training;
 
-namespace FlightJournal.Web.Models;
-
-public class PilotDetailsViewModel 
+namespace FlightJournal.Web.Models
 {
-    public PilotDetailsViewModel()
+    public class PilotDetailsViewModel
     {
-        
-    }
-    public PilotDetailsViewModel(Pilot pilot, IQueryable<PilotInTrainingProgram> trainingProgramsForPilot)
-    {
-        this.PilotId = pilot.PilotId;
-        this.ClubId = pilot.ClubId;
-        this.ClubName = pilot.Club.ShortName;
-        this.Name = pilot.Name;
-        this.Email = pilot.Email;
-        this.MemberId = pilot.MemberId;
-        this.MobilNumber = pilot.MobilNumber;
-        this.UnionId = pilot.UnionId;
-        this.InstructorId = pilot.InstructorId;
-        this.ExitDate = pilot.ExitDate;
+        public PilotDetailsViewModel()
+        {
 
-        this.TrainingPrograms = trainingProgramsForPilot.ToList().Select(x=>new PilotInTrainingProgramViewModel(x)).ToArray();
-    }
+        }
+        public PilotDetailsViewModel(Pilot pilot, IQueryable<PilotInTrainingProgram> trainingProgramsForPilot)
+        {
+            this.PilotId = pilot.PilotId;
+            this.ClubId = pilot.ClubId;
+            this.ClubName = pilot.Club.ShortName;
+            this.Name = pilot.Name;
+            this.Email = pilot.Email;
+            this.MemberId = pilot.MemberId;
+            this.MobilNumber = pilot.MobilNumber;
+            this.UnionId = pilot.UnionId;
+            this.InstructorId = pilot.InstructorId;
+            this.ExitDate = pilot.ExitDate;
 
-    public int PilotId { get; set; }
-    public int ClubId { get; set; }
-    public string ClubName { get; set; }
+            this.TrainingPrograms = trainingProgramsForPilot.ToList().Select(x => new PilotInTrainingProgramViewModel(x)).ToArray();
+        }
 
-    public string Name { get; set; }
-    public string Email{ get; set; }
-    public string MemberId{ get; set; }
-    public string MobilNumber{ get; set; }
-    public string UnionId{ get; set; }
-    public string InstructorId{ get; set; }
+        public int PilotId { get; set; }
+        public int ClubId { get; set; }
+        public string ClubName { get; set; }
 
-    public DateTime? ExitDate { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MemberId { get; set; }
+        public string MobilNumber { get; set; }
+        public string UnionId { get; set; }
+        public string InstructorId { get; set; }
 
-    public void Update(Pilot p)
-    {
-        p.PilotId = PilotId;
-        p.ClubId = ClubId;
-        p.Name = Name;
-        p.Email = Email;
-        p.MemberId = MemberId;
-        p.MobilNumber = MobilNumber;
-        p.UnionId = UnionId;
-        p.InstructorId = InstructorId;
-        p.ExitDate = ExitDate;
-    }
+        public DateTime? ExitDate { get; set; }
 
-    public PilotInTrainingProgramViewModel[] TrainingPrograms{ get; set; } 
-}
+        public void Update(Pilot p)
+        {
+            p.PilotId = PilotId;
+            p.ClubId = ClubId;
+            p.Name = Name;
+            p.Email = Email;
+            p.MemberId = MemberId;
+            p.MobilNumber = MobilNumber;
+            p.UnionId = UnionId;
+            p.InstructorId = InstructorId;
+            p.ExitDate = ExitDate;
+        }
 
-public class PilotInTrainingProgramViewModel
-{
-    public PilotInTrainingProgramViewModel()
-    {
-        
-    }
-    public PilotInTrainingProgramViewModel(PilotInTrainingProgram  pitp)
-    {
-        PilotId = pitp.Pilot.PilotId;
-        ProgramId = pitp.Program.Training2ProgramId;
-        ProgramName = pitp.Program.Name;
-        StartDate = pitp.StartDate;
-        EndDate = pitp.EndDate;
-        Completed = pitp.IsCompleted;
-
-    }
-    public int PilotId { get; set; }
-    public int ProgramId { get; set; }
-    public string ProgramName { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-    public bool Completed { get; set; }
-
-    public void Update(PilotInTrainingProgram pitp)
-    {
-        pitp.EndDate = EndDate;
-        pitp.IsCompleted = Completed;
+        public PilotInTrainingProgramViewModel[] TrainingPrograms { get; set; }
     }
 
+    public class PilotInTrainingProgramViewModel
+    {
+        public PilotInTrainingProgramViewModel()
+        {
+
+        }
+        public PilotInTrainingProgramViewModel(PilotInTrainingProgram pitp)
+        {
+            PilotId = pitp.Pilot.PilotId;
+            ProgramId = pitp.Program.Training2ProgramId;
+            ProgramName = pitp.Program.Name;
+            StartDate = pitp.StartDate;
+            EndDate = pitp.EndDate;
+            Completed = pitp.IsCompleted;
+
+        }
+        public int PilotId { get; set; }
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool Completed { get; set; }
+
+        public void Update(PilotInTrainingProgram pitp)
+        {
+            pitp.EndDate = EndDate;
+            pitp.IsCompleted = Completed;
+        }
+
+    }
 }
